66e7ef45d6a33a7ff0e28ca61476e4910bf94fc1
alysiawon <won.alysia@gmail.com>
2021-09-10 13:43:48 -0400

    Update

--- Dashboard/static/js/logic.js
+++ Dashboard/static/js/logic.js
@@ -1,21 +1,61 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 2);
+// ---------- BASEMAP LAYER ----------//
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: API_KEY
-}).addTo(mymap);
+var myMap = L.map("mapid", {
+	center: [45.52,-122.67],
+	zoom:12
+})
 
-var layerGroup = L.layerGroup().addTo(mymap);
+// Satellite Layer
+var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/satellite-v9",
+  accessToken: API_KEY
+}).addTo(myMap);
 
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
+// Light Layer
+var light = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/light-v10",
+  accessToken: API_KEY
+}).addTo(myMap);
 
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
+// Dark Layer
+var dark = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/dark-v10",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Layers
+var baseMaps = {
+	Satellite: satellite,
+	Light: light,
+	Dark: dark
+  };
+
+L.control.layers(baseMaps).addTo(myMap);
+
+// ---------- MEDALS LAYER ----------//
+
+function olympicsSize(m) {
+    return m > 1000 ? m*150 :
+        m > 500 ? m*250 :
+        m > 100 ? m*500 :
+        m*1000
+}
+
+function olympicsColor(m) {
+    return m > 800 ? '#FBB32E' :
+        m > 400 ? '#0186C3' :
+        m > 200 ? '#158C39' :
+        '#EE304D'
+}
