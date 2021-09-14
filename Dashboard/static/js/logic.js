9b8674c1b5b91ece5e684836f16c7a3fc9081512
alysiawon <won.alysia@gmail.com>
2021-09-13 21:40:44 -0400

    Update

--- Dashboard/static/js/logic.js
+++ Dashboard/static/js/logic.js
@@ -3,13 +3,16 @@ let countryData = countryFile;
 
 // ---------- INPUT BOX ----------//
 
+// ---------- Table For History ----------//
+
+// Build Table References 
 var tbody = d3.select("tbody");
+
 function buildTable(data) {
-  // First, clear out any existing data
   tbody.html("");
   // Next, loop through each object in the data
   // and append a row and cells for each value in the row
-  countryData.forEach((dataRow) => {
+  data.forEach((dataRow) => {
     // Append a row to the table body
     let row = tbody.append("tr");
     // Loop through each field in the dataRow and add
@@ -21,45 +24,53 @@ function buildTable(data) {
   });
 }
 
-// ---------- Table For History ----------//
-
-// 1. Create a variable to keep track of all the filters as an object.
+// Create Filter
 var filters = {}
-// 3. Use this function to update the filters. 
 function updateFilters() {
-    // Save the element that was changed as a variable.
+  // Save the element, value and id of the changed value
     var element = d3.select(this);
-    // Save the value that was changed as a variable.
     var elementvalue = element.property("value");
-    // Save the id of the filter that was changed as a variable.
     var elementid = element.attr("id");
-    // If a filter value was entered then add that filterId and value
-    // to the filters list. Otherwise, clear that filter from the filters object.
+
+    // If Statement for year to be an integer
+    if (elementid = "year") {
+      elementvalue = parseInt(elementvalue)
+    }
+
+    // If Statement to add value to list or ignore 
     if (elementvalue) {
       filters[elementid] = elementvalue;
     }
     else {
       delete filters[elementid]
     }
-    // Call function to apply all filters and rebuild the table
+
     filterTable();
   }
-  // Use this function to filter the table when data is entered.
+
+  // Build Table with Filtered Data
   function filterTable() {
-    // Set the filtered data to the tableData.
     var filteredData = countryData;
-    // Loop through all of the filters and keep any data that
-    // matches the filter values
+
     Object.entries(filters).forEach(([key, value]) => {
       filteredData = filteredData.filter(row => row[key] === value);
     })
-    // Finally, rebuild the table using the filtered data
-    buildTable(countryData)
+
+    // Rebuild The Data
+    buildTable(filteredData)
   }
-  // Attach an event to listen for changes to each filter
+  // Attach Event To Listen To Data
   d3.selectAll("input").on("change", updateFilters)
-  // Build the table when the page loads
+
+  // Build New Table
   buildTable(countryData);
+
+  // Build Bar Graph
+
+  // Build Map
+
+
+
 
 // ---------- Bar Graph Ranking Winning Country By Medals ----------//
 
@@ -69,7 +80,7 @@ var sortedMedals = countryData.sort((a,b) => b.gold_medals - a.gold_medals);
 var slicedMedals = sortedMedals.slice(0,50);
 var reverseMedals = slicedMedals.reverse();
 var finalMedals = reverseMedals.map(obj => obj.gold_medals)
-
+console.log(finalMedals)
 // Y Variable - Country Names
 var countryName = reverseMedals.map(obj => obj.country_name);
 
@@ -156,7 +167,7 @@ let overlays = {
 
 // var medalsLayer, gdpLayer, populationLayer;
 
-L.control.layers(baseMaps, overlays).addTo(map);
+// L.control.layers(baseMaps, overlays).addTo(map);
 
 // ---------- MAP LAYER ----------//
 
