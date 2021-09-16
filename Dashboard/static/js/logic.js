8c17041283fa93b725f561cb6371d16ecdf713df
alysiawon <won.alysia@gmail.com>
2021-09-15 20:45:05 -0400

    Update README

--- Dashboard/static/js/logic.js
+++ Dashboard/static/js/logic.js
@@ -92,173 +92,78 @@ function filterChart(filteredData){
 
 function buildMap(data){
   var coordinates = data.map(d => [d.latitude, d.longitude])
-  console.log(coordinates)
-}
 
-let mapboxLink = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}";
+  let mapboxLink = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}";
 
-// Satellite Layer
-let satellite = L.tileLayer(mapboxLink, {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/satellite-streets-v11",
-  accessToken: API_KEY
-})
+  // Satellite Layer
+  let satellite = L.tileLayer(mapboxLink, {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/satellite-streets-v11",
+    accessToken: API_KEY
+  })
 
-// Light Layer
-let light = L.tileLayer(mapboxLink, {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/light-v10",
-  accessToken: API_KEY
-})
+  // Light Layer
+  let light = L.tileLayer(mapboxLink, {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+  })
 
-// Dark Layer
-let dark = L.tileLayer(mapboxLink, {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/dark-v10",
-  accessToken: API_KEY
-})
+  // Dark Layer
+  let dark = L.tileLayer(mapboxLink, {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/dark-v10",
+    accessToken: API_KEY
+  })
 
-// Initiatlize Map
-let map = L.map("mapid", {
-	center: [25,5],
-	zoom:1.5,
-	layer: [satellite],
-})
+  // Initiatlize Map
+  let map = L.map("mapid", {
+    center: [25,5],
+    zoom:2,
+    layer: [satellite],
+  })
 
-// Layers
-let baseMaps = {
-	"Satellite": satellite,
-	"Light": light,
-	"Dark": dark
+  // Layers
+  let baseMaps = {
+    "Satellite": satellite,
+    "Light": light,
+    "Dark": dark
+    };
+
+  // Add Second Layer
+  let medalsData = new L.LayerGroup();
+  // let gdpData = new L.LayerGroup();
+  // let populationData = new L.LayerGroup();
+
+  let overlays = {
+    "Medals": medalsData,
+    // "GDP": gdpData,
+    // "Population": populationData
   };
 
-// Add Second Layer
-let medalsData = new L.LayerGroup();
-let gdpData = new L.LayerGroup();
-let populationData = new L.LayerGroup();
+  L.control.layers(baseMaps, overlays).addTo(map);
 
-let overlays = {
-  "Medals": medalsData,
-  "GDP": gdpData,
-  "Population": populationData
-};
+  // Drop Points
 
-// var medalsLayer, gdpLayer, populationLayer;
+  // Create Pop Up
+  // L.control.layers() {
+  // pointToLayer: function(feature, latlng) {
+  //   console.log(data);
+  //   return L.circleMarker(latlng);
+  // },
 
-L.control.layers(baseMaps, overlays).addTo(map);
-
-
-// ---------- MAP LAYER ----------//
-
-
-// Create Function for Year Change
-
-let filter_year = "";
-
-function changeYear(element) {
-    filter_year = element.value;
+  // onEachFeature: function(feature, layer) {
+  //   layer.bindPopup("Country Name: " + feature.properties.country_name + "<br>Number of Medals: " + feature.properties.gold_medals);
+  // }
 }
 
-// Function for Lat Long
-
-function latlong() {
-
-}
-
-// Function for Pop Up Based on Click
-function filteredMap() {
-  map.on('click', function(e) {        
-    var popLocation= e.latlng;
-    var popup = L.popup()
-    .setLatLng(popLocation)
-    .setContent('<p>Hello world!<br />This is a nice popup.</p>')
-    .openOn(map);        
-});
-}
-
-
-function onEachFeature(feature, layer) {
-  var coords = new Array();
-  coords.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]]);
-  var popupContent = " <strong>Name:</strong> " +
-      feature.properties.Name +
-      " <br /><br /> " +
-      " <strong>Description:</strong> " +
-      feature.properties.description +
-      " <br /><br /> " +
-      " <strong>LAT/LONG:</strong> " +
-      " <a href='https://www.google.com/maps/search/?api=1&query=" + feature.geometry.coordinates[1] + "," + feature.geometry.coordinates[0] + "'" + " target='_blank'>" + feature.geometry.coordinates[1] + "," + feature.geometry.coordinates[0] + "</a>" +
-      " <br /><br /> " +
-      "<strong>Geometry Type:</strong> " +
-      feature.geometry.type;
-
-  if (feature.properties && feature.properties.popupContent) {
-      popupContent += feature.properties.popupContent;
-  }
-
-  layer.bindPopup(popupContent);
-
-}
-// function styleInfo(feature) {
-//   return {
-//     opacity: 1,
-//     fillOpacity: 1,
-//     fillColor: getColor(feature.properties.mag),
-//     color: "#000000",
-//     radius: getRadius(feature.properties.mag),
-//     stroke: true,
-//     weight: 0.5
-//   };
-// }
-
-//   // This function determines the color of the marker based on the magnitude of the earthquake.
-//   function getColor(medals) {
-//     if (medals > 50) {
-//       return "#ea2c2c";
-//     }
-//     if (medals > 40) {
-//       return "#ea822c";
-//     }
-//     if (medals > 30) {
-//       return "#ee9c00";
-//     }
-//     if (medals > 20) {
-//       return "#eecc00";
-//     }
-//     if (medals > 10) {
-//       return "#d4ee00";
-//     }
-//     return "#98ee00";
-//   }
-
-//   function getRadius(medals) {
-//     if (medals === 0) {
-//       return 1;
-//     }
-//     return medals * 4;
-//   }
-
-//   // // Creating a GeoJSON layer with the retrieved data.
-//   // L.geoJson(data, {
-//   //   // We turn each feature into a circleMarker on the map.
-//   //   pointToLayer: function(feature, latlng) {
-//   //       console.log(data);
-//   //       return L.circleMarker(latlng);
-//   //     },
-//   //   // We set the style for each circleMarker using our styleInfo function.
-//   // style: styleInfo,
-//   //  // We create a popup for each circleMarker to display the magnitude and location of the earthquake
-//   //  //  after the marker has been created and styled.
-//   //  onEachFeature: function(feature, layer) {
-//   //   layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-//   // }
-// // }).addTo(medalsData);
+// buildMap(countryData);
