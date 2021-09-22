da2ee126cc56e91e06f9aa4c3bc506774cd33c08
alysiawon <won.alysia@gmail.com>
2021-09-21 23:16:46 -0400

    Update

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,1 +1,49 @@
-app.py
+# Dependencies
+import numpy as np
+import pandas as pd
+import json
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+from config import password
+
+# Set Up Database
+host = "olympic-databases.cqg15afinz5u.us-east-2.rds.amazonaws.com"
+engine = create_engine(f"postgresql://postgres:{password}@{host}:5432/olympic-databases")
+# Base = automap_base()
+# Base.prepare(engine, reflect=True)
+# Tokyo = Base.classes.tokyo
+# Medals = Base.classes.medals
+# session = Session(engine)
+connection = engine.connect()
+
+# Set Up Flask
+app = Flask(__name__)
+
+# Create Flask Routes
+@app.route("/api/v1.0/medals")
+def medals():
+    # import SQL table as pandas dataframe
+    medals_df = pd.read_sql('select * from medals', connection)
+    
+    # convert pandas dataframe to json
+    medals_json = json.dumps(medals_df.to_dict('records'))
+    
+    return medals_json
+
+@app.route("/api/v1.0/tokyo")
+def tokyo():
+    # import SQL table as pandas dataframe
+    tokyo_df = pd.read_sql('select * from tokyo', connection)
+    
+    # convert pandas dataframe to json
+    tokyo_json = json.dumps(tokyo_df.to_dict('records'))
+    
+    return tokyo_json
+    
+
+if __name__ == "__main__":
+       app.run(debug=True)
--- Dashboard/static/js/logic.js
+++ Dashboard/static/js/logic.js
@@ -1,5 +1,12 @@
-// The Data From medals.js
-let countryData = countryTest;
+// // The Data From medals.js
+// let countryData = countryTest;
+
+// let countryData = d3.json("http://127.0.0.1:5000/api/v1.0/tokyo")
+//   .then(function(data){
+//   console.log(data)
+// });
+
+d3.json("http://127.0.0.1:5000/api/v1.0/tokyo")
 
 // ---------- Table For History ----------//
 
@@ -15,10 +22,11 @@ function buildTable(data) {
     let row = tbody.append("tr");
     // Loop through each field in the dataRow and add
     // each value as a table cell (td)
-    Object.values(dataRow).forEach((val) => {
-      let cell = row.append("td");
-      cell.text(val);
-
+    Object.entries(dataRow).forEach(([key, val]) => {
+      if ((key != "latitude") & (key != "longitude")) {
+        let cell = row.append("td");
+        cell.text(val);
+      }
     });
   });
 }
@@ -29,47 +37,49 @@ function buildTable(data) {
 var filters = {}
 function updateFilters() {
   // Save the element, value and id of the changed value
-    var element = d3.select(this);
-    var elementvalue = element.property("value");
-    var elementid = element.attr("id");
+  var element = d3.select(this);
+  var elementvalue = element.property("value");
+  var elementid = element.attr("id");
 
-    // If Statement for year to be an integer
-    if (elementid = "year") {
-      elementvalue = parseInt(elementvalue)
-    }
-
-    // If Statement to add value to list or ignore 
-    if (elementvalue) {
-      filters[elementid] = elementvalue;
-    }
-    else {
-      delete filters[elementid]
-    }
-
-    var filteredData = countryData;
-
-    Object.entries(filters)
-    .forEach(([key, value]) => {filteredData = filteredData.filter(row => row[key] === value)
-    .filter(medals => medals.gold_medals >= 1)
-    .sort((a,b) => b.gold_medals - a.gold_medals);})
-
-    buildTable(filteredData);
-    filterChart(filteredData);
-    buildMap(filteredData);
+  // If Statement for year to be an integer
+  if (elementid = "year") {
+    elementvalue = parseInt(elementvalue)
   }
+
+  // If Statement to add value to list or ignore 
+  if (elementvalue) {
+    filters[elementid] = elementvalue;
+  }
+  else {
+    delete filters[elementid]
+  }
+
+  var filteredData = countryData;
+
+  Object.entries(filters)
+    .forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value)
+        .filter(medals => medals.gold_medals >= 1)
+        .sort((a, b) => b.gold_medals - a.gold_medals);
+    })
+
+  buildTable(filteredData);
+  filterChart(filteredData);
+  buildMap(filteredData);
+}
 
 // Attach Event To Listen To Data
 d3.selectAll("input").on("change", updateFilters)
 
 // Build New Table
-buildTable(countryData);
+// buildTable(countryData);
 
 // ---------- Bar Graph ----------//
 
 // Build Bar Graph with Filtered Data
-function filterChart(filteredData){
-  var filteredMedals =filteredData.map(d => d.gold_medals)
-  var filteredCountry =filteredData.map(d => d.country_name)
+function filterChart(filteredData) {
+  var filteredMedals = filteredData.map(d => d.gold_medals)
+  var filteredCountry = filteredData.map(d => d.country_name)
 
   // Bar Graph
   var trace = {
@@ -82,14 +92,14 @@ function filterChart(filteredData){
   var chartData = [trace];
 
   var layout = {
-  title: "Top Medals By Country in Year"
+    title: "Top Medals By Country in Year"
   }
 
   Plotly.newPlot("bar-plot", chartData, layout)
-}  
+}
 
 // ---------- Map ----------//
-function buildMap(data){
+function buildMap(data) {
   // --- Map Set Up --- //
 
   // Street Layer
@@ -108,9 +118,9 @@ function buildMap(data){
 
   // Dark Layer
   let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      accessToken: API_KEY
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
   });
 
   // Initalize Map
@@ -134,7 +144,7 @@ function buildMap(data){
   };
 
   // Show Map
-  L.control.layers(baseMaps, overlays).addTo(map);
+  // L.control.layers(baseMaps, overlays).addTo(map);
 
   // Legend
 
@@ -142,34 +152,28 @@ function buildMap(data){
   // --- Map Set Up --- //
 
   // Plot Coordinates
-  for (var i=0; i<data.length;i++){
+  for (var i = 0; i < data.length; i++) {
 
     var countryName = data.map(d => d.country_name);
     var goldMedal = data.map(d => d.gold_medals);
     var gdp = data.map(d => d.gdp);
     var population = data.map(d => d.population);
     var latlng = data.map(d => [d.latitude, d.longitude]);
-    
-    var marker = L.marker(latlng[i]).addTo(mapMedals)
+
+    var marker = L.marker(latlng[i]).addTo(map)
     marker.bindPopup(
-      "<b>Country Name: </b>" + countryName[i]+
-      "<br><b>Number of Gold Medals: </b>" + goldMedal[i] + 
+      "<b>Country Name: </b>" + countryName[i] +
+      "<br><b>Number of Gold Medals: </b>" + goldMedal[i] +
       "<br><b>Population: </b>" + population[i] +
       "<br><b>GDP: </b>" + gdp[i]).openPopup();
 
-    // var circle = L.circle(latlng[i], {
-    //   color: "red",
-    //   fillColor: "#f03",
-    //   fillOpacity: 0.5,
-    //   radius: 1600,
-    // }).addTo(mapMedals);
   }
 
 
 
 
 
-  
+
 }
 
 
